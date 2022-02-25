ad8f89d3ea6fa59cf0f152d00b279dc94a5bee2e
OpeDada <opedada@gmail.com>
2022-02-25 21:39:27 +0900

    implement form

--- pages/registration.js
+++ pages/registration.js
@@ -1,7 +1,16 @@
 import React from "react"
+import styles from '../styles/Registration.module.css'
 
 export default function Registration(){
   return (
-    <div></div>
+    <div className="styles.container">
+      <form>
+        <label>
+          Full Name:
+          <input type="text"></input>
+        </label>
+      </form>
+
+    </div>
   )
 }
