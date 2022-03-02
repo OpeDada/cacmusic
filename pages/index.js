d247fabe6e179ca6f4b718f6e049d21a0d2b93b6
OpeDada <opedada@gmail.com>
2022-03-02 11:02:23 +0900

    import styles

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 
 import React from "react"
 import Registration from "./registration"
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles}>
       <Registration />
 
     </div>
